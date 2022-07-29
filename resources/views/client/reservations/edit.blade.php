2e3ebf79358be33ded20d6bbb4612e6b5825f9d6
jibulya0920 <webdev980101@gmail.com>
2022-07-30 02:26:44 +0900

    test

--- resources/views/client/reservations/edit.blade.php
+++ resources/views/client/reservations/edit.blade.php
@@ -158,7 +158,7 @@
                             {{ $arr[$i] .  $priceName->name }}
                         </td>
 
-                        {{dd(strtolower($tmp_arr[$reservations->price_type].'_'.$i))}}
+                        {{dd(strtolower($tmp_arr[$reservations->price_type].'_'. ((int)$i + 1)))}}
                         <td style="text-align: right;">{{ number_format($prices[0][strtolower($tmp_arr[$reservations->price_type].'_'.$i+1)]) }} 円</td>
                         <input type="hidden" id="price{{$i + 1}}" value="{{ $prices[0][strtolower($tmp_arr[$reservations->price_type].'_'.$i+1)] }}">
                        
