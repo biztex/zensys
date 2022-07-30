ccab54fee7b014fbf90e465bd9935eee10adcd94
jibulya0920 <webdev980101@gmail.com>
2022-07-30 10:05:16 +0900

    mail test

--- resources/views/user/reservations/reqemail.blade.php
+++ resources/views/user/reservations/reqemail.blade.php
@@ -27,7 +27,7 @@
   $tmp_arr=["A","B","C","D","E","F","G","H","H","J","K","L"];
   for ($i=0; $i<count($arr); $i++){
     echo $arr[$i] . $priceName->name . " / " . $prices[0][strtolower($tmp_arr[$reservation->price_type]."_". ((int)$i + 1))]  . " 円 × " . $reservation->{'type' . $i . '_number'} . "=" . number_format((int)$prices[0][strtolower($tmp_arr[$reservation->price_type]."_". ((int)$i + 1))]  * (int)$reservation->{'type' . $i . '_number'}) . " 円" . "\n";
-    $amount += (int)$prices[0][strtolower($tmp_arr[$reservation->price_type].'_'. ((int)$i + 1))];
+    $amount += (int)$prices[0][strtolower($tmp_arr[$reservation->price_type]."_". ((int)$i + 1))]  * (int)$reservation->{'type' . $i . '_number'} ; 
   }
 
 @endphp
