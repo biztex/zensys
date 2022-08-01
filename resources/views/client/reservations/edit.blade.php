6dbe3d3aeef8c927df4f04371a5c6eb98b170407
lancersmascot <webdev980101@gmail.com>
2022-08-01 21:01:25 +0900

    modified reservavtion management page

--- resources/views/client/reservations/edit.blade.php
+++ resources/views/client/reservations/edit.blade.php
@@ -131,6 +131,218 @@
                             </div>
                         </div>
                     <div class="form-group row mt-3 bg-dark">
+                        <label class="col-md-10 col-form-label text-md-left"><span class="h5"><i class="fas fa-users"></i> 旅行参加者情報</span></label>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right">参加者(代表者)氏名(漢字)</label>
+                        <div class="row align-items-center col-md-3">
+                            <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('姓') }}</label>
+                            <div class="col ml-2">
+                                <input id="" type="text" class="form-control" name="" value="">
+                            </div>
+                        </div>
+                        <div class="row align-items-center col-md-3">
+                            <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('名') }}</label>
+                            <div class="col ml-2">
+                                <input id="" type="text" class="form-control" name="" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right">参加者(代表者)氏名(カナ)</label>
+                        <div class="row align-items-center col-md-3">
+                            <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('セイ') }}</label>
+                            <div class="col ml-2">
+                                <input id="" type="text" class="form-control" name="" value="">
+                            </div>
+                        </div>
+                        <div class="row align-items-center col-md-3">
+                            <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('メイ') }}</label>
+                            <div class="col ml-2">
+                                <input id="" type="text" class="form-control" name="" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('年齢') }}</label>
+                        <div class="col-md-3">
+                            <input id="" type="text" class="form-control" name="" value="" >
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('生年月日') }}</label>
+                        <div class="col-md-3">
+                            <input id="" type="date" class="form-control" name="" value="">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('郵便番号') }}</label>
+                        <div class="col-md-3">
+                            <input id="" type="text" class="form-control" name="postal_code" value="" onKeyUp="AjaxZip3.zip2addr(this,'','prefecture','address');">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> 都道府県</label>
+                        <div class="col-md-3">
+                            <select class="form-control" name="prefecture">
+                                <option value="">選択してください</option>
+                                {{-- <option value="北海道" @if(old('prefecture',$users->prefecture)=='北海道') selected @endif>北海道</option>
+                                <option value="青森県" @if(old('prefecture',$users->prefecture)=='青森県') selected @endif>青森県</option>
+                                <option value="岩手県" @if(old('prefecture',$users->prefecture)=='岩手県') selected @endif>岩手県</option>
+                                <option value="宮城県" @if(old('prefecture',$users->prefecture)=='宮城県') selected @endif>宮城県</option>
+                                <option value="秋田県" @if(old('prefecture',$users->prefecture)=='秋田県') selected @endif>秋田県</option>
+                                <option value="山形県" @if(old('prefecture',$users->prefecture)=='山形県') selected @endif>山形県</option>
+                                <option value="福島県" @if(old('prefecture',$users->prefecture)=='福島県') selected @endif>福島県</option>
+                                <option value="茨城県" @if(old('prefecture',$users->prefecture)=='茨城県') selected @endif>茨城県</option>
+                                <option value="栃木県" @if(old('prefecture',$users->prefecture)=='栃木県') selected @endif>栃木県</option>
+                                <option value="群馬県" @if(old('prefecture',$users->prefecture)=='群馬県') selected @endif>群馬県</option>
+                                <option value="埼玉県" @if(old('prefecture',$users->prefecture)=='埼玉県') selected @endif>埼玉県</option>
+                                <option value="千葉県" @if(old('prefecture',$users->prefecture)=='千葉県') selected @endif>千葉県</option>
+                                <option value="東京都" @if(old('prefecture',$users->prefecture)=='東京都') selected @endif>東京都</option>
+                                <option value="神奈川県" @if(old('prefecture',$users->prefecture)=='神奈川県') selected @endif>神奈川県</option>
+                                <option value="新潟県" @if(old('prefecture',$users->prefecture)=='新潟県') selected @endif>新潟県</option>
+                                <option value="富山県" @if(old('prefecture',$users->prefecture)=='富山県') selected @endif>富山県</option>
+                                <option value="石川県" @if(old('prefecture',$users->prefecture)=='石川県') selected @endif>石川県</option>
+                                <option value="福井県" @if(old('prefecture',$users->prefecture)=='福井県') selected @endif>福井県</option>
+                                <option value="山梨県" @if(old('prefecture',$users->prefecture)=='山梨県') selected @endif>山梨県</option>
+                                <option value="長野県" @if(old('prefecture',$users->prefecture)=='長野県') selected @endif>長野県</option>
+                                <option value="岐阜県" @if(old('prefecture',$users->prefecture)=='岐阜県') selected @endif>岐阜県</option>
+                                <option value="静岡県" @if(old('prefecture',$users->prefecture)=='静岡県') selected @endif>静岡県</option>
+                                <option value="愛知県" @if(old('prefecture',$users->prefecture)=='愛知県') selected @endif>愛知県</option>
+                                <option value="三重県" @if(old('prefecture',$users->prefecture)=='三重県') selected @endif>三重県</option>
+                                <option value="滋賀県" @if(old('prefecture',$users->prefecture)=='滋賀県') selected @endif>滋賀県</option>
+                                <option value="京都府" @if(old('prefecture',$users->prefecture)=='京都府') selected @endif>京都府</option>
+                                <option value="大阪府" @if(old('prefecture',$users->prefecture)=='大阪府') selected @endif>大阪府</option>
+                                <option value="兵庫県" @if(old('prefecture',$users->prefecture)=='兵庫県') selected @endif>兵庫県</option>
+                                <option value="奈良県" @if(old('prefecture',$users->prefecture)=='奈良県') selected @endif>奈良県</option>
+                                <option value="和歌山県" @if(old('prefecture',$users->prefecture)=='和歌山県') selected @endif>和歌山県</option>
+                                <option value="鳥取県" @if(old('prefecture',$users->prefecture)=='鳥取県') selected @endif>鳥取県</option>
+                                <option value="島根県" @if(old('prefecture',$users->prefecture)=='島根県') selected @endif>島根県</option>
+                                <option value="岡山県" @if(old('prefecture',$users->prefecture)=='岡山県') selected @endif>岡山県</option>
+                                <option value="広島県" @if(old('prefecture',$users->prefecture)=='広島県') selected @endif>広島県</option>
+                                <option value="山口県" @if(old('prefecture',$users->prefecture)=='山口県') selected @endif>山口県</option>
+                                <option value="徳島県" @if(old('prefecture',$users->prefecture)=='徳島県') selected @endif>徳島県</option>
+                                <option value="香川県" @if(old('prefecture',$users->prefecture)=='香川県') selected @endif>香川県</option>
+                                <option value="愛媛県" @if(old('prefecture',$users->prefecture)=='愛媛県') selected @endif>愛媛県</option>
+                                <option value="高知県" @if(old('prefecture',$users->prefecture)=='高知県') selected @endif>高知県</option>
+                                <option value="福岡県" @if(old('prefecture',$users->prefecture)=='福岡県') selected @endif>福岡県</option>
+                                <option value="佐賀県" @if(old('prefecture',$users->prefecture)=='佐賀県') selected @endif>佐賀県</option>
+                                <option value="長崎県" @if(old('prefecture',$users->prefecture)=='長崎県') selected @endif>長崎県</option>
+                                <option value="熊本県" @if(old('prefecture',$users->prefecture)=='熊本県') selected @endif>熊本県</option>
+                                <option value="大分県" @if(old('prefecture',$users->prefecture)=='大分県') selected @endif>大分県</option>
+                                <option value="宮崎県" @if(old('prefecture',$users->prefecture)=='宮崎県') selected @endif>宮崎県</option>
+                                <option value="鹿児島県" @if(old('prefecture',$users->prefecture)=='鹿児島県') selected @endif>鹿児島県</option>
+                                <option value="沖縄県" @if(old('prefecture',$users->prefecture)=='沖縄県') selected @endif>沖縄県</option> --}}
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('住所') }}</label>
+                        <div class="col-md-6">
+                            <input id="" type="text" class="form-control" name="address" value="">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label text-md-right"> {{ __('マンション・ビル名') }}</label>
+                        <div class="col-md-6">
+                            <input id="" type="text" class="form-control" name="" value="">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('電話番号') }}</label>
+                        <div class="col-md-6">
+                            <input id="" type="text" class="form-control" name="" value="">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right">乗車地</label>
+                        <div class="col-md-3">
+                            <select name="" id="" class="form-control">
+                                <option value="乗車地">乗車地</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right">降車地</label>
+                        <div class="col-md-3">
+                            <select name="" id="" class="form-control">
+                                <option value="降車地">降車地</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group row border-top pt-1">
+                        <label for="" class="col-md-3 col-form-label text-md-right">同行者氏名(漢字)</label>
+                        <div class="row align-items-center col-md-3">
+                            <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('姓') }}</label>
+                            <div class="col ml-2">
+                                <input id="" type="text" class="form-control" name="" value="">
+                            </div>
+                        </div>
+                        <div class="row align-items-center col-md-3">
+                            <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('名') }}</label>
+                            <div class="col ml-2">
+                                <input id="" type="text" class="form-control" name="" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right">同行者氏名(カナ)</label>
+                        <div class="row align-items-center col-md-3">
+                            <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('セイ') }}</label>
+                            <div class="col ml-2">
+                                <input id="" type="text" class="form-control" name="" value="">
+                            </div>
+                        </div>
+                        <div class="row align-items-center col-md-3">
+                            <label class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('メイ') }}</label>
+                            <div class="col ml-2">
+                                <input id="" type="text" class="form-control" name="" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('年齢') }}</label>
+                        <div class="col-md-3">
+                            <input id="" type="text" class="form-control" name="" value="" >
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('生年月日') }}</label>
+                        <div class="col-md-3">
+                            <input id="" type="date" class="form-control" name="" value="">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right"><span class="badge badge-danger">必須</span> {{ __('性別') }}</label>
+                        <div class="col-md-3">
+                            <select name="" id="" class="form-control">
+                                <option value="男">男</option>
+                                <option value="女">女</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right">乗車地</label>
+                        <div class="col-md-3">
+                            <select name="" id="" class="form-control">
+                                <option value="乗車地">乗車地</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-md-3 col-form-label text-md-right">降車地</label>
+                        <div class="col-md-3">
+                            <select name="" id="" class="form-control">
+                                <option value="降車地">降車地</option>
+                            </select>
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="form-group row mt-3 bg-dark">
                         <label class="col-md-10 col-form-label text-md-left"><span class="h5"><i class="fas fa-fw fa-th-list"></i> 料金情報</span></label>
                     </div>
                     <div class="form-group row mb-2 float-right">
@@ -160,17 +372,17 @@
 
                         <td style="text-align: right;">{{ number_format($prices[0][strtolower($tmp_arr[$reservations->price_type].'_'. ((int)$i + 1))]) }} 円</td>
                         <input type="hidden" id="price{{$i + 1}}" value="{{ $prices[0][strtolower($tmp_arr[$reservations->price_type].'_'. ((int)$i + 1))] }}">
-                       
+
                         <td style="text-align: right; padding-left: 50px;"><div class="row justify-content-center"><input id="per-number{{ ($i + 1) }}" class="number-input col-md-5" name="type{{$i}}_number" value="@php echo $reservations->{'type' . $i . '_number'};@endphp"> <span style="line-height: 1.8;" class="col-md-2"> 名</span></div></td>
                         <input type="hidden" class="col-md-6 text-right" id="number{{$i + 1}}" value="@php echo $reservations->{'type' . $i . '_number'};@endphp">
 
                         <td id="per-text-price{{ ($i + 1) }}" style="text-align: right;">
                         @php
-                      
+
                         $type_number = $reservations->{'type' . $i . '_number'};
-                       
+
                         $result = $type_number * $prices[0][strtolower($tmp_arr[$reservations->price_type].'_'. ((int)$i + 1))];
-                     
+
                         echo number_format($result) . ' 円';
                         @endphp
                         </td>
@@ -212,12 +424,12 @@ $(document).ready(function(){
     for (var i = 1 ; i <= 6 ; i++) {
         if ($('#per-number' + i).val()) {
             var tmpPerNumber = $.trim($('#per-number' + i).val()),
-                perNumber = parseInt(tmpPerNumber); 
+                perNumber = parseInt(tmpPerNumber);
                 totalNumber += perNumber;
         }
         if ($('#per-price' + i).val()) {
             var tmpPerPrice = $.trim($('#per-price' + i).val()),
-                perPrice = parseInt(tmpPerPrice); 
+                perPrice = parseInt(tmpPerPrice);
                 totalPrice += perPrice;
         }
     }
@@ -230,10 +442,10 @@ $('.update-price').click(function() {
     for (var i = 1 ; i <= 6 ; i++) {
         if ($('#per-number' + i).val()) {
             var tmpPerNumber = $.trim($('#per-number' + i).val()),
-                perNumber = parseInt(tmpPerNumber); 
+                perNumber = parseInt(tmpPerNumber);
             totalNumber += perNumber;
             var tmpPrice = $.trim($('#price' + i).val()),
-                price = parseInt(tmpPrice); 
+                price = parseInt(tmpPrice);
             price = perNumber * price;
             $('#per-text-price' + i).text(price.toLocaleString() + " 円");
                 totalPrice += price;
@@ -260,4 +472,3 @@ $('.submit-send').click(function() {
 });
 </script>
 @stop
-
