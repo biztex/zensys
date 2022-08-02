c5d6460bb4b4ca469ef56c615f37a2f7c2d9de05
jibulya0920 <webdev980101@gmail.com>
2022-08-02 23:52:32 +0900

    fixed reservation number on form

--- resources/views/user/reservations/create.blade.php
+++ resources/views/user/reservations/create.blade.php
@@ -93,7 +93,7 @@
 
                                                     echo $arr[$i-1] . $priceType['name'] . " / " . number_format($stock_price_type[strtolower($stock['rank']) . '_'. $i]) ." 円";
                                                     echo '<input type="hidden" name="price_table[]" value="'. $arr[$i-1] . $priceType['name'] . '/' . number_format($stock_price_type[strtolower($stock['rank']) . '_'. $i]) .' 円' .'">';
-                                                    echo '<p><input type="text" name="type_number[]" class="number" min="0" max="'.$stock->limit_number.'" value="1"> 人</p>';
+                                                    echo '<p><input type="text" name="type_number[]" class="number" min="0" max="'.$stock->limit_number.'" value="0"> 人</p>';
                                                     echo '</div>';
                                                 }
                                                 echo '</td></tr>';
