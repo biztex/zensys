e208afa391e8b539c79285cdc2acab4f28261e4e
jibulya0920 <webdev980101@gmail.com>
2022-08-10 22:43:16 +0900

    fixed some error

--- public/list.php
+++ public/list.php
@@ -68,7 +68,7 @@
                         $params = $_GET['kind'];
                      }
 
-                    $url = "http://153.127.31.62/zenryo/public/api/plans/json/" . $params;
+                    $url = "https://zenryo.zenryo-ec.info/api/plans/json/" . $params;
                     $json = file_get_contents($url);
                     $plans = json_decode($json,true);
 
