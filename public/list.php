e4a2fd5466bd92a7a01a77f0bdc322ee70c82c0b
jibulya0920 <webdev980101@gmail.com>
2022-08-10 23:13:00 +0900

    remove bug

--- public/list.php
+++ public/list.php
@@ -73,8 +73,6 @@
                     $json = file_get_contents($url);
                     $plans = json_decode($json,true);
 
-                     var_dump($plans);
-                     exit;
                     $j = 0;
 
                     if(count($plans) > 0){
