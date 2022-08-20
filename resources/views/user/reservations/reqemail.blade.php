0d8bb38be367aefef59ebcbf39fe8f7da06afb59
jibulya0920 <webdev980101@gmail.com>
2022-08-20 15:32:18 +0900

    test mail

--- resources/views/user/reservations/reqemail.blade.php
+++ resources/views/user/reservations/reqemail.blade.php
@@ -29,8 +29,6 @@
     for($k=0; $k<count($tmp_arr); $k++){
       if(array_key_exists(sprintf('type%d_%s_%d_number', $type_id,$k,$i+1), $Number_of_reservations)){
         echo $arr[$i] . $priceName->name . " / " . $prices[0][$tmp_arr[$k]."_". ((int)$i + 1)]  . " 円 × " . $Number_of_reservations->{sprintf('type%d_%s_%d_number', $type_id,$tmp_arr[$k],$i+1)} . "=" . number_format((int)$prices[0][$tmp_arr[$k]."_". ((int)$i + 1)]  * (int)$Number_of_reservations->{sprintf('type%d_%s_%d_number', $type_id,$tmp_arr[$k],$i+1)}) . " 円" . "\n";
-      }else{
-        echo $arr[$i] . $priceName->name . " / " . $prices[0][$tmp_arr[$k]."_". ((int)$i + 1)]  . " 円 × " . 0 . "=" . number_format((int)$prices[0][$tmp_arr[$k]."_". ((int)$i + 1)]  * 0) . " 円" . "\n";
       }
     }
   }
