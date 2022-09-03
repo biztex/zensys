43139fb20a638983c921a95ce6656fc59c97a171
biztex <luxuryman887@gmail.com>
2022-09-03 09:57:26 +0900

    test reqemail

--- resources/views/user/reservations/reqemail.blade.php
+++ resources/views/user/reservations/reqemail.blade.php
@@ -25,12 +25,6 @@
   $Number_of_reservations = json_decode($reservation->Number_of_reservations);
   $arr = ['大人　','子供　','幼児　'];
   $tmp_arr=['a','b','c','d','e','f','g','h','i','j','k','l'];
-  for($i=0; $i<count($arr); $i++){
-    if(array_key_exists(sprintf('type%d_%s_%d_number', $type_id,$tmp_arr[$reservation->price_type],$i+1), $Number_of_reservations)){
-      echo $arr[$i] . $priceName->name . " / " . $prices[0][$tmp_arr[$reservation->price_type]."_". ((int)$i + 1)]  . " 円 × " . $Number_of_reservations->{sprintf('type%d_%s_%d_number', $type_id,$tmp_arr[$reservation->price_type],$i+1)} . "=" . number_format((int)$prices[0][$tmp_arr[$reservation->price_type]."_". ((int)$i + 1)]  * (int)$Number_of_reservations->{sprintf('type%d_%s_%d_number', $type_id,$tmp_arr[$reservation->price_type],$i+1)}) . " 円" . "\n";
-    }else{
-      echo $arr[$i] . $priceName->name . " / " . $prices[0][$tmp_arr[$reservation->price_type]."_". ((int)$i + 1)]  . " 円 × " . 0 . "=" . number_format((int)$prices[0][$tmp_arr[$reservation->price_type]."_". ((int)$i + 1)]  * 0) . " 円" . "\n";
-    }
-  }
+
 @endphp
 
