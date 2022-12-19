770f485f2fd61889430e765d3b78eba199beffe6
biztex <luxuryman887@gmail.com>
2022-12-19 17:38:07 +0900

    change button style

--- public/html/reservation-companion.php
+++ public/html/reservation-companion.php
@@ -1,5 +1,5 @@
 
-    <div class="d-flex align-items-center mb-2">
+    <div class="d-flex align-items-center mb-2 flex-wrap justify-content-center justify-content-md-start">
         <span class="requiredRed if-non-member">同行者がいない場合は×で入力枠を閉じて、確認画面へ進んでください。</span>
         <div class="ml-3"><a type="button" class="grayBtn copyAddress helperTextOver">参加者と同行者がの住所が同一の場合<br>こちらをクリック</a></div>
     </div>
