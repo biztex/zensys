14e1992adebf20972762fffc070c721506b410e8
biztex <luxuryman887@gmail.com>
2022-12-19 17:05:45 +0900

    chagne style

--- public/html/reservation-companion.php
+++ public/html/reservation-companion.php
@@ -1,7 +1,7 @@
 
-    <div class="d-flex align-items-center justify-content-between mb-3">
+    <div class="d-flex align-items-center mb-2">
         <span class="requiredRed if-non-member">同行者がいない場合は×で入力枠を閉じて、確認画面へ進んでください。</span>
-        <div class="mr-5"><button type="button" class="grayBtn copyAddress helperTextOver">参加者と同行者がの住所が同一の場合<br>こちらをクリック</button></div>
+        <div class="ml-3"><button type="button" class="grayBtn copyAddress helperTextOver">参加者と同行者がの住所が同一の場合<br>こちらをクリック</button></div>
     </div>
     <table class="reserveTable">
         <tr class="nameTr">
